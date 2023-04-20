991fa87b74e3c65a9efba7cd1b6fbaa533c7b3a6
sabra derraz <sabraderraz@gmail.com>
2023-04-20 07:52:19 -0700

    fix main description and warnings

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - entry point , a C program that prints the size of various types on the computer it's compiled and run on
+ * main - entry point
  *
  * Return : Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
 	return (0);
 }
