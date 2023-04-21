4f27046e4a409d11c0d970a107b1e359cd818b78
sabra derraz <sabraderraz@gmail.com>
2023-04-21 16:07:01 -0700

    add file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible
+ * different combos of 3 digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 48;
+	int j = 49;
+	int k = 50;
+
+	while (i <= 56)
+	{
+		while (j <= 57)
+		{
+			while (k <= 58)
+			{
+				if (j > i)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+		k = j + 1;
+	}
+	putchar('\n');
+	return (0);
+}
