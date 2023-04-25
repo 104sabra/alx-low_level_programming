7d82850e17e7d8d9232a181b5d45b1621c31dd47
sabra derraz <sabraderraz@gmail.com>
2023-04-25 10:17:59 -0700

    three

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,15 @@
 
 /**
  * _islower - checks for lowercase character
+ * @c - parameter
+ * Return: 1 if c is lowercase, otherwise 0
  */
 
 int _islower(int c)
 {
 	while (c >= 'a' && c <= 'z')
 	{
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
