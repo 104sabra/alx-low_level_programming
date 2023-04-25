1fa17d8efbb64ba480a49902c4c45fe7963a9ccd
sabra derraz <sabraderraz@gmail.com>
2023-04-25 09:42:54 -0700

    one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabt -  function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
