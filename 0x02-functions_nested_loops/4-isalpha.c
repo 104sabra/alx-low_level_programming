d3e0ea480e87bde4b3b84362b9ab605a3b3b1027
sabra derraz <sabraderraz@gmail.com>
2023-04-25 10:37:01 -0700

    fou

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: this is the variable to be checked
+ * Return: 1 if c is alphabetic, otherwise 0
+ */
+
+int _islower(int c)
+{
+	while ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
