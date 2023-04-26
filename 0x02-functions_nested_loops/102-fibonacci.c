0b6034999dea27920f044b25ec73c5c2495f3dc0
sabra derraz <sabraderraz@gmail.com>
2023-04-26 14:22:29 -0700

    14 fix 1

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,29 +7,24 @@
 
 int main(void)
 {
-	int count;
+	int count = 1;
 	int fib1 = 1;
 	int fib2 = 2;
 	int x;
 
-	printf("%i," fib1);
+	printf("%i, ", fib1);
 
-	while (count <= 50)
+	while (count < 50)
 	{
-		if (count != 50)
-		{
-			printf("%i, ", fib2);
-		}
-		else
-		{
-			printf("%i\n", fib2);
-		}
+		printf("%i, ", fib2);
 
 		x = fib2;
 		fib2 = fib2 + fib1;
 		fib1 = x;
 		count++;
 	}
+
+	printf("%i\n", fib2);
 
 	return (0);
 }
