925121704b38a92230b91014f6ab8dea121f933a
sabra derraz <sabraderraz@gmail.com>
2023-04-27 10:28:42 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_number - prints the numbers from 0 to 9
+ * print_most_numbers - prints the numbers from 0 to 9
  * not printing 2 and 4
  * Return: void
  */
