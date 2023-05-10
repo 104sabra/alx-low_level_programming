edd750daa98766d17433f034d939af48ad79a58c
sabra derraz <sabraderraz@gmail.com>
2023-05-10 15:33:44 -0700

    8 advanced prep for tmr

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * wildcmp - checks if two strings are identical
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if identical 0 otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+
+}
