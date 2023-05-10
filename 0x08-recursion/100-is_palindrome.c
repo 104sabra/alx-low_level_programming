19284006632f1f1a876ca4fb88a698f7de237558
sabra derraz <sabraderraz@gmail.com>
2023-05-10 15:29:34 -0700

    7 advanced prep for tmr

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is palindrome or not
+ * @s: string
+ * Return: 1 if palindrome 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+
+}
