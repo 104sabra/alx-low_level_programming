c9643ff56789b4d40d8278e239061ec8e92b1173
cramaillotte <sabraderraz@gmail.com>
2023-08-07 20:29:53 +0100

    one fix one

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IREAD | S_IWRITE);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 
 	if (fd == -1)
 		return (-1);
