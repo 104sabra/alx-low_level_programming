29e5181e9867b6d9ae0486f84cd248cb6fd69d57
sabra derraz <sabraderraz@gmail.com>
2023-08-11 13:57:38 -0700

    fix betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-	
 	if (text_content)
 	{
 		while (text_content[i])
